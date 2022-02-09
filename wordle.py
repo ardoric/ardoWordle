0833e379eca3fe3acdcc5ad7421e536e6faa759d
Ricardo Silva <ardoric@gmail.com>
2022-02-09 19:15:23 +0000

    fixed match algorithm

--- wordle.py
+++ wordle.py
@@ -5,15 +5,19 @@ import sys
 
 
 def match(known, guess):
-        res = ''
-        for (c1,c2) in zip(known, guess):
-                if c1 == c2:
-                        res += 'g'
-                elif c2 in known:
-                        res += 'y'
-                else:
-                        res += 'b'
-        return res
+        res = list('b'*len(known))
+	known = list(known)
+	# green pass
+	for (i,c) in enumerate(guess):
+		if c == known[i]:
+			res[i] = 'g'
+			known[i] = '_'
+	# yellow pass
+	for (i,c) in enumerate(guess):
+		if res[i] != 'g' and c in known:
+			res[i] = 'y'
+			known[known.index(c)] = '_'
+        return ''.join(res)
 
 def trimwords(wordlist, guess, res):
 	return [ w for w in wordlist if match(w, guess) == res ]
