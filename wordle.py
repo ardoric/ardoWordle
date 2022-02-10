e7ff6b351f2c2e52ca15a507d7136ddf1d1d823d
Ricardo Silva <ardoric@gmail.com>
2022-02-10 12:18:58 +0000

    added comments

--- wordle.py
+++ wordle.py
@@ -3,22 +3,38 @@ import json
 import random
 import sys
 
-
+# provide the comparison between a known / target word and a guess
+# outputs a string with the following codes:
+# b - letter is not in the word in any spot
+# g - letter is in the word in the same spot
+# y - letter in in the word in a different spot
+# further considerations are that letters on the target word are only "used" once
+# they can't both be used for g and y, for example, which is why we do two passes
 def match(known, guess):
+	# ugly hacks for addressable "strings"
         res = list('b'*len(known))
 	known = list(known)
+
 	# green pass
+	# mark all letters in the same place
 	for (i,c) in enumerate(guess):
 		if c == known[i]:
 			res[i] = 'g'
 			known[i] = '_'
+	
 	# yellow pass
+	# mark yellows and make sure to remove already used letters from target
 	for (i,c) in enumerate(guess):
 		if res[i] != 'g' and c in known:
 			res[i] = 'y'
 			known[known.index(c)] = '_'
+	
+	# undo the hack
+	# to those unfamiliar this creates a string by joining all the strings
+	# in a list separated by ''
         return ''.join(res)
 
+# returns a filtered wordlist with all the potential answers to a give guess
 def trimwords(wordlist, guess, res):
 	return [ w for w in wordlist if match(w, guess) == res ]
 
